f1aaea0891c70a987dfc92f5701634ed5fc0ac18
karminer60 <karminer60@gmail.com>
2021-06-20 22:35:57 -0700

    fixing warnings

--- cooker-app/src/Home.js
+++ cooker-app/src/Home.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Categories from "./Categories.js";
 
 //set up containers for: categories, recipes, and recipe
-export default function Home(){
-    return(<>
-        <div className="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand">Simple Tomatoe</a>
-             
-        </div>
+export default function Home() {
+    return (<>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+            <a href="https://www.google.com/" class="navbar-brand">Simple Tomatoe</a>
+            
+
+        </nav>
         <div className="container" >
 
-            <Categories/>
+            <Categories />
 
         </div>
-        </>
+    </>
     )
 }
