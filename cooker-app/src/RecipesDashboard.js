f496f714e1020a154e2b7b51400ef42503da7003
karminer60 <karminer60@gmail.com>
2024-09-02 20:59:43 -0700

    adds fetch request in Recipe Dashboar

--- cooker-app/src/RecipesDashboard.js
+++ cooker-app/src/RecipesDashboard.js
@@ -3,13 +3,26 @@ import NavBar from './NavBar';
 import { Link } from 'react-router-dom';
 
 export default function RecipesDashboard() {
+    //JavaScript
+    let recipes = fetch("http://localhost:8081/recipes")
+                .then(data => {
+                    //do something with the data
+                })
+                .catch(error => {
+                    //do something with the error
+                })
+    //React
     return (
         <div className="container-fluid">
             <NavBar />
             <div style={{ padding: "5rem" }}>
-
-                Recipes dashboard goes here
-
+                Make a fetch request for all recipes, here
+               
+                {
+                 recipes.array.forEach(element => {
+                    console.log(element)
+            
+                })}
             </div>
 
 
